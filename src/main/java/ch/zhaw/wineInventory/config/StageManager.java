cf88d18150720ef2dc91f5de13ef5abbfd1eb984
Christian Jeitziner <chde@jeitziner.com>
2019-01-12 18:11:22 +0100

    Fullscreen Problem behoben

--- src/main/java/ch/zhaw/wineInventory/config/StageManager.java
+++ src/main/java/ch/zhaw/wineInventory/config/StageManager.java
@@ -92,12 +92,9 @@ public class StageManager {
 	 */
 	private void show(final Parent rootnode, String title) {
 		Scene scene = prepareScene(rootnode);
-		// scene.getStylesheets().add("/styles/Styles.css");
 
-		// primaryStage.initStyle(StageStyle.TRANSPARENT);
 		primaryStage.setTitle(title);
 		primaryStage.setScene(scene);
-		primaryStage.sizeToScene();
 		primaryStage.centerOnScreen();
 
 		try {
