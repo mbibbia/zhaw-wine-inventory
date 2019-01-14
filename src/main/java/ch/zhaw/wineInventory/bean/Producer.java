cf40ef4933d3dddeca89469e704636877796cf88
marco <marco@bibbias-XPS-13>
2019-01-14 21:49:05 +0100

    Producer Entity changed relation for country and region

--- src/main/java/ch/zhaw/wineInventory/bean/Producer.java
+++ src/main/java/ch/zhaw/wineInventory/bean/Producer.java
@@ -6,7 +6,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -46,10 +46,10 @@ public class Producer {
 
 	private String url;
 
-	@OneToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	private Country country;
 
-	@OneToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	private Region region;
 
 	/**
