0c2d77eab9aecf05c9a08596dac62c65d5e58fbf
marco <marco@bibbias-XPS-13>
2019-01-17 21:49:14 +0100

    ServiceImpl Integration Test WineService

--- src/test/java/ch/zhaw/wineInventory/test/service/WineServiceImplIntegrationTest.java
+++ src/test/java/ch/zhaw/wineInventory/test/service/WineServiceImplIntegrationTest.java
@@ -1,6 +1,12 @@
 package ch.zhaw.wineInventory.test.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +17,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ch.zhaw.wineInventory.bean.Classification;
 import ch.zhaw.wineInventory.bean.Wine;
+import ch.zhaw.wineInventory.repository.ClassificationRepository;
 import ch.zhaw.wineInventory.repository.WineRepository;
 import ch.zhaw.wineInventory.service.WineService;
 import ch.zhaw.wineInventory.service.impl.WineServiceImpl;
@@ -34,13 +42,18 @@ public class WineServiceImplIntegrationTest {
 
 	@MockBean
 	private WineRepository wineRepository;
+	@MockBean
+	private ClassificationRepository classificationRepository;
 
 	@Before
 	public void setUp() {
 		Wine wine = new Wine();
+		List<Wine> wines = new ArrayList<Wine>();
 		wine.setName("Barolo");
+		wine.setClassification(classificationRepository.findByName("DOCG"));
 
 		Mockito.when(wineRepository.findByName(wine.getName())).thenReturn(wine);
+		Mockito.when(wineRepository.findByClassification(wine.getClassification())).thenReturn(wines);
 
 	}
 
@@ -52,6 +65,26 @@ public class WineServiceImplIntegrationTest {
 
 		assertEquals(found.getName(), name);
 
+	}
+	
+	@Test
+	public void whenValidClassification_thenWineShouldBeFound() {
+
+		Classification classification = classificationRepository.findByName("DOCG");
+		
+		assertNotNull(wineService.findByClassification(classification));
+
+	}
+	
+	@Test
+	public void whenWineDeleted_thenWineNotFound() {
+		
+		String name = "Barolo";
+		Wine found = wineService.findByName(name);
+		wineService.delete(found);
+		
+		assertNull(wineService.find(found.getId()));
+		
 	}
 
 }
